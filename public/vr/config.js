1f3f74de05def2658954ed6cdac68e46a365dbdf
Andrei Roman <43176471+urbandrei@users.noreply.github.com>
2026-01-18 12:52:20 +0000

    fixed config bug

--- public/vr/config.js
+++ public/vr/config.js
@@ -1,4 +1,4 @@
 // Game server URL configuration
 // Update this URL to point to your Oracle Cloud VM once it's set up
 // Example: 'http://<YOUR_VM_IP>:3000'
-window.GAME_SERVER_URL = 'https://pirate-jam-2026.onrender.com';
+window.GAME_SERVER_URL = 'https://www.urbandrei.com';
