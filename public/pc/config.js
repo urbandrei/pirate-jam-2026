2e4e868dc6f665f0597c959e42b8d755ac66cdac
Andrei Roman <43176471+urbandrei@users.noreply.github.com>
2026-01-18 12:04:00 +0000

    dns fix?

--- public/pc/config.js
+++ public/pc/config.js
@@ -1,4 +1,4 @@
 // Game server URL configuration
 // Update this URL to point to your Oracle Cloud VM once it's set up
 // Example: 'http://<YOUR_VM_IP>:3000'
-window.GAME_SERVER_URL = 'http://147.224.138.170:3000';
+window.GAME_SERVER_URL = 'https://www.urbandrei.com';
